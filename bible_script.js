0fb5d97e0164c6deef5ddfa69a563b7306e8700b
Ieuan Jones <ieuan@ieuan.wales>
2023-10-17 13:16:25 +0100

    Now you can choose your reading plan

---
+++ bible_script.js
@@ -0,0 +1,555 @@
+BOOKS = [
+    {
+        'name': 'Genesis',
+        'chapters': 50,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': 'Exodus',
+        'chapters': 40,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': 'Leviticus',
+        'chapters': 27,
+        'section': 'old',
+        'weight': 1.5
+    },
+    {
+        'name': 'Numbers',
+        'chapters': 36,
+        'section': 'old',
+        'weight': 1.5
+    },
+    {
+        'name': 'Deuteronomy',
+        'chapters': 34,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': 'Joshua',
+        'chapters': 24,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Judges',
+        'chapters': 21,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': 'Ruth',
+        'chapters': 4,
+        'section': 'old',
+        'weight': 4
+    },
+    {
+        'name': '1 Samuel',
+        'chapters': 31,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': '2 Samuel',
+        'chapters': 24,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': '1 Kings',
+        'chapters': 22,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': '2 Kings',
+        'chapters': 25,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': '1 Chronicles',
+        'chapters': 29,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': '2 Chronicles',
+        'chapters': 36,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': 'Ezra',
+        'chapters': 10,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': 'Nehemiah',
+        'chapters': 13,
+        'section': 'old',
+        'weight': 3
+    },
+    {
+        'name': 'Esther',
+        'chapters': 10,
+        'section': 'old',
+        'weight': 4
+    },
+    {
+        'name': 'Job',
+        'chapters': 42,
+        'section': 'wisdom',
+        'weight': 1
+    },
+    {
+        'name': 'psalm',
+        'chapters': 150,
+        'section': 'psalm',
+        'weight': 1
+    },
+    {
+        'name': 'Proverbs',
+        'chapters': 31,
+        'section': 'wisdom',
+        'weight': 1
+    },
+    {
+        'name': 'Ecclesiastes',
+        'chapters': 12,
+        'section': 'wisdom',
+        'weight': 1
+    },
+    {
+        'name': 'Song of Solomon',
+        'chapters': 8,
+        'section': 'wisdom',
+        'weight': 1
+    },
+    {
+        'name': 'Isaiah',
+        'chapters': 66,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': 'Jeremiah',
+        'chapters': 52,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Lamentations',
+        'chapters': 5,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Ezekiel',
+        'chapters': 48,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Daniel',
+        'chapters': 12,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': 'Hosea',
+        'chapters': 14,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Joel',
+        'chapters': 3,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Amos',
+        'chapters': 9,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Obadiah',
+        'chapters': 1,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Jonah',
+        'chapters': 4,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Micah',
+        'chapters': 7,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Nahum',
+        'chapters': 3,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Habakkuk',
+        'chapters': 3,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Zephaniah',
+        'chapters': 3,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Haggai',
+        'chapters': 2,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Zechariah',
+        'chapters': 14,
+        'section': 'old',
+        'weight': 1
+    },
+    {
+        'name': 'Malachi',
+        'chapters': 4,
+        'section': 'old',
+        'weight': 2
+    },
+    {
+        'name': 'Matthew',
+        'chapters': 28,
+        'section': 'new',
+        'weight': 3
+    },
+    {
+        'name': 'Mark',
+        'chapters': 16,
+        'section': 'new',
+        'weight': 3
+    },
+    {
+        'name': 'Luke',
+        'chapters': 24,
+        'section': 'new',
+        'weight': 3
+    },
+    {
+        'name': 'John',
+        'chapters': 21,
+        'section': 'new',
+        'weight': 3
+    },
+    {
+        'name': 'Acts',
+        'chapters': 28,
+        'section': 'new',
+        'weight': 3
+    },
+    {
+        'name': 'Romans',
+        'chapters': 16,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '1 Corinthians',
+        'chapters': 16,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '2 Corinthians',
+        'chapters': 13,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Galatians',
+        'chapters': 6,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Ephesians',
+        'chapters': 6,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Philippians',
+        'chapters': 4,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Colossians',
+        'chapters': 4,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '1 Thessalonians',
+        'chapters': 5,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '2 Thessalonians',
+        'chapters': 3,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '1 Timothy',
+        'chapters': 6,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '2 Timothy',
+        'chapters': 4,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Titus',
+        'chapters': 3,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Philemon',
+        'chapters': 1,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Hebrews',
+        'chapters': 13,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'James',
+        'chapters': 5,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '1 Peter',
+        'chapters': 5,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '2 Peter',
+        'chapters': 3,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '1 John',
+        'chapters': 5,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '2 John',
+        'chapters': 1,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': '3 John',
+        'chapters': 1,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Jude',
+        'chapters': 1,
+        'section': 'new',
+        'weight': 1
+    },
+    {
+        'name': 'Revelation',
+        'chapters': 22,
+        'section': 'new',
+        'weight': 1
+    }
+]
+SECTION_NAMES = ['old', 'new', 'psalm', 'wisdom'];
+
+
+function get_start() {
+    return new Date(localStorage.getItem('start'));
+}
+
+
+function generate_section(name) {
+    return {
+        name: name,
+        books: [],
+        total: 0,
+        weighted_total: 0
+    }
+}
+
+
+function generate_sections(days) {
+    const sections = {};
+    const reading = {};
+
+    for (const section of SECTION_NAMES) {
+        sections[section] = generate_section(section);
+    }
+
+    for (const book of BOOKS) {
+        sections[book.section].books.push(book);
+        sections[book.section].total = sections[book.section].books.reduce(
+            (count, book) => count + book.chapters, 0
+        );
+        sections[book.section].weighted_total = sections[book.section].books.reduce(
+            (count, book) => count + book.chapters * 1/book.weight, 0
+        );
+
+    }
+    for (const section of SECTION_NAMES) {
+        reading[section] = calculate_section(sections[section], days);
+    }
+
+    return reading;
+}
+
+
+function calculate_section(section, days) {
+    const chapters_per_day = section.weighted_total / days;
+
+    let total = 0;
+    let goal = 0;
+
+    let book = 0;
+    let chapter = 1;
+
+    const reading = [];
+
+    for (let i=0; i<days; i++) {
+        const day = [];
+        goal += chapters_per_day;
+
+        while (total < goal) {
+            total += 1/section.books[book].weight;
+            day.push(`${section.books[book].name} ${chapter}`);
+
+            chapter += 1;
+            if (chapter > section.books[book].chapters) {
+                chapter = 1;
+                book += 1;
+
+                if (book >= section.books.length) {
+                    total = goal;
+                }
+            }
+
+            if (chapters_per_day < 1) {
+                break;
+            }
+        }
+
+        reading.push(day);
+        if (book >= section.books.length) {
+            break;
+        }
+    }
+
+    return reading;
+}
+
+
+function render_section(section_name, section, day) {
+    if (day >= section.length || section[day] === []) {
+        return '';
+    }
+
+    let out = '';
+    for (const chapter of section[day]) {
+        out += `<li class="${section_name}" />${chapter}`
+    }
+
+    return out
+}
+
+
+function calculate() {
+    localStorage.setItem('start', document.getElementById('start').valueAsDate);
+    localStorage.setItem('days', document.getElementById('days').value);
+    localStorage.setItem('calculated', 'true');
+
+    const days = document.querySelector('#days').value;
+    const reading = generate_sections(days);
+    const day = new Date(get_start());
+    let out = '';
+
+    for (let i=0; i<days; i++) {
+        out += `<div id="day${i+1}" class="reading"><h1>${MONTHS[day.getMonth()]} ${day.getDate()} (Day ${i+1})</h1><ul>`;
+        for (const section of SECTION_NAMES) {
+            out += render_section(section, reading[section], i);
+        }
+        out += '</div>';
+        day.setDate(day.getDate() + 1);
+    }
+
+    document.querySelector('#results').innerHTML = out;
+    do_scroll();
+}
+
+
+/* Auxiliary */
+
+function do_scroll() {
+    const day_no = Math.floor((today - get_start())/86400000)+1;
+    let to_scroll = document.getElementById('day'+day_no);
+    if (to_scroll === null) {
+        to_scroll = document.getElementById('day1');
+    }
+
+    window.scrollTo({
+        top: to_scroll.offsetTop - 140,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+window.onload = function () {
+    if (localStorage.getItem('calculated')) {
+        document.getElementById('start').valueAsDate = get_start();
+        document.getElementById('days').value = localStorage.getItem('days');
+        setTimeout(do_scroll, 100);
+        calculate();
+    } else {
+        document.getElementById('start').valueAsDate = today;
+        document.getElementById('days').value = 365;
+    }
+}
+
+
+const today = new Date();
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
